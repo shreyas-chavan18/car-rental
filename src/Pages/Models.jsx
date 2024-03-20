a731503e1f996237aa77c124f2887ded395f5505
shreyas-chavan18 <shreyaschavan18502@gmail.com>
2024-03-20 16:53:12 +0530

    first commit

---
+++ src/Pages/Models.jsx
@@ -0,0 +1,296 @@
+import Footer from "../components/Footer";
+import HeroPages from "../components/HeroPages";
+import CarImg1 from "../images/cars-big/altroz.png";
+import CarImg2 from "../images/cars-big/celerio.png";
+import CarImg3 from "../images/cars-big/ciaz.png";
+import CarImg4 from "../images/cars-big/nexon.png";
+import CarImg5 from "../images/cars-big/BMW_330i_ind.png";
+import CarImg6 from "../images/cars-big/harrier.png";
+import { Link } from "react-router-dom";
+import { IconCar, IconPhone, IconStar } from "@tabler/icons-react";
+
+function Models() {
+  return (
+    <>
+      <section className="models-section">
+        <HeroPages name="Vehicle Models" />
+        <div className="container">
+          <div className="models-div">
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg1} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Altroz</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>Rs 7000</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; TATA
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Petrol / Diesel &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg2} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Celerio</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>Rs 5000</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; Maruti Suzuki
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Petrol / Diesel &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg3} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Ciaz</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>Rs 6000</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; Maruti Suzuki
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Petrol &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg4} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Nexon</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>Rs 10000</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; TATA
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Manual / Automatic
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Petrol / Diesel / EV &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg5} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>BMW 330i</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>Rs 15000</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; BMW
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg6} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Harrier</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>Rs 12000</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; TATA
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Petrol / Diesel &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="book-banner">
+          <div className="book-banner__overlay"></div>
+          <div className="container">
+            <div className="text-content">
+              <h2>Book a car by getting in touch with us</h2>
+              <span>
+                <IconPhone width={40} height={40} />
+                <h3>+91 1234567890</h3>
+              </span>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </section>
+    </>
+  );
+}
+
+export default Models;
---
+++ src/components/CarData.js
@@ -0,0 +1,100 @@
+import Golf6 from "../images/cars-big/golf6.jpg";
+import AudiA1 from "../images/cars-big/audia1.jpg";
+import Toyota from "../images/cars-big/toyotacamry.jpg";
+import Bmw320 from "../images/cars-big/bmw320.jpg";
+import Benz from "../images/cars-big/benz.jpg";
+import Passat from "../images/cars-big/passatcc.jpg";
+
+import Altroz from "../images/cars-big/altroz.png"
+import Celerio from "../images/cars-big/celerio.png"
+import Ciaz from "../images/cars-big/ciaz.png"
+import Nexon from "../images/cars-big/nexon.png"
+import BMW330i from "../images/cars-big/BMW_330i_ind.png"
+import Harrier from "../images/cars-big/harrier.png"
+
+export const CAR_DATA = [
+  [
+    {
+      name: "TATA Altroz",
+      price: "7k",
+      img: Altroz,
+      model: "Altroz",
+      mark: "TATA",
+      year: "2020",
+      doors: "4",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Petrol / Diesel",
+    },
+  ],
+  [
+    {
+      name: "Celerio",
+      price: "5k",
+      img: Celerio,
+      model: "Celerio",
+      mark: "Maruti Suzuki",
+      year: "2018",
+      doors: "4",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Petrol / Diesel",
+    },
+  ],
+  [
+    {
+      name: "Ciaz",
+      price: "6k",
+      img: Ciaz,
+      model: "Ciaz",
+      mark: "Maruti Suzuki",
+      year: "2016",
+      doors: "4",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Petrol",
+    },
+  ],
+  [
+    {
+      name: "Nexon",
+      price: "10k",
+      img: Nexon,
+      model: "Nexon",
+      mark: "TATA",
+      year: "2021",
+      doors: "4",
+      air: "Yes",
+      transmission: "Manual / Automatic",
+      fuel: "Petrol / Diesel / EV",
+    },
+  ],
+  [
+    {
+      name: "BMW 3 series",
+      price: "15k",
+      img: BMW330i,
+      model: "BMW 330i",
+      mark: "BMW",
+      year: "2018",
+      doors: "4",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Harrier",
+      price: "12k",
+      img: Harrier,
+      model: "Harrier",
+      mark: "TATA",
+      year: "2020",
+      doors: "4",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Petrol / Diesel",
+    },
+  ],
+];
